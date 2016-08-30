6c68ff0b115b23c73743ef647b040080ae7d8d36
Jochen Rauschenbusch <jochen.rauschenbusch@bridging-it.de>
2016-08-30 12:35:29 +0200

    Bring all unit tests to pass

--- test/fixtures/umd/expected.js
+++ test/fixtures/umd/expected.js
@@ -2,7 +2,11 @@
   if ('function' === 'function' && true) {
     // factory is called  with "$__exports" in the case where "exports" is required by AMD, OR when using AMD "function" form
     System.registerDynamic(['cjs.js'], false, function ($__require, $__exports, $__module) {
-      return factory.call($__exports, $__require, $__exports, $__require('cjs.js'));
+      if (typeof factory === 'function') {
+        return factory.call($__exports, $__require, $__exports, $__require('cjs.js'));
+      } else {
+        return factory;
+      }
     });
   } else if (typeof exports === 'object') {
     module.exports = factory(require, exports, module);
